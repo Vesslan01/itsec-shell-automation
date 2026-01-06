8e04bd6804373730d0c51987c37ea59d8f0487fb
Vesslan01 <adam.wesslen@gmail.com>
2026-01-06 22:03:40 +0100

    chore: add analysis_engine draft

---
+++ analysis_engine.py
@@ -0,0 +1,20 @@
+def load_all_data():
+    ...
+
+def analyze_users():
+    ...
+
+def analyze_ips():
+    ...
+
+def generate_report():
+    ...
+
+def main():
+    load_all_data()
+    analyze_users()
+    analyze_ips()
+    generate_report()
+
+if __name__ == "__main__":
+    main()
